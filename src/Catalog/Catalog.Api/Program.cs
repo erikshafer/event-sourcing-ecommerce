1dc1da23c2802c432e50357bd021f318bf376839
Erik Shafer <erik.shafer@gmail.com>
2024-05-09 14:26:30 -0500

    feat; moved logging to its own Infrastructure extension method (Seq)

--- src/Catalog/Catalog.Api/Program.cs
+++ src/Catalog/Catalog.Api/Program.cs
@@ -1,4 +1,5 @@
 using Catalog.Api;
+using Catalog.Api.Infrastructure;
 using Eventuous.Spyglass;
 using Microsoft.AspNetCore.Http.Json;
 using NodaTime;
@@ -6,18 +7,9 @@ using NodaTime.Serialization.SystemTextJson;
 using Serilog;
 using Serilog.Events;
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Verbose()
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-    .MinimumLevel.Override("Grpc", LogEventLevel.Information)
-    .MinimumLevel.Override("Grpc.Net.Client.Internal.GrpcCall", LogEventLevel.Error)
-    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc.Infrastructure", LogEventLevel.Warning)
-    .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .WriteTo.Seq("http://localhost:5341")
-    .CreateLogger();
-
 var builder = WebApplication.CreateBuilder(args);
+
+Logging.ConfigureLog(builder.Configuration);
 builder.Host.UseSerilog();
 
 builder.Services
