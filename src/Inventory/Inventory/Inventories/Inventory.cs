d59d717163c7f4b66a454bad8003ee0bdcacb338
Erik Shafer <erik.shafer@gmail.com>
2024-06-19 16:17:23 -0500

    query endpoint for InventoryState

--- src/Inventory/Inventory/Inventories/Inventory.cs
+++ src/Inventory/Inventory/Inventories/Inventory.cs
@@ -6,34 +6,6 @@ namespace Inventory.Inventories;
 
 public class Inventory : Aggregate<InventoryState>
 {
-    public async Task Initialize(
-        string inventoryId,
-        string sku,
-        Services.IsInventoryAvailableBySku isInventoryAvailableBySku)
-    {
-        EnsureDoesntExist();
-        await EnsureSkuAvailable(new Sku(sku), isInventoryAvailableBySku);
-
-        Apply(new V1.InventoryInitialized(
-            inventoryId,
-            sku));
-    }
-
-    public void StockingFromProcurementOrder(
-        string inventoryId,
-        string procurementOrderId,
-        int quantityStocked,
-        DateTimeOffset stockedAt)
-    {
-        EnsureExists();
-
-        Apply(new V1.InventoryStockedFromProcurementOrder(
-            inventoryId,
-            procurementOrderId,
-            quantityStocked
-        ));
-    }
-
     private static async Task EnsureSkuAvailable(
         Sku sku,
         Services.IsInventoryAvailableBySku isInventoryAvailableBySku)
