3d12a05a1e25ea7bc1d254f0c4595edb3fd6d28f
Erik Shafer <erikshafer@users.noreply.github.com>
2024-06-19 10:26:10 -0500

    Readme update (2024-06-19) (#42)
    * readme update; modules, technologies used
    
    * table of contents anchors, typos, etc
    
    * added Serilog for Legacy, need to clean up OTel still
    
    * added JVM to runtimes as a TODO

--- src/Legacy/Legacy.Api/Program.cs
+++ src/Legacy/Legacy.Api/Program.cs
@@ -1,13 +1,17 @@
+using Legacy.Api.Infrastructure;
 using Legacy.Data;
 using Legacy.Data.DbContexts;
 using Legacy.Data.Seeds;
-using Microsoft.EntityFrameworkCore;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Logging.ConfigureLog(builder.Configuration);
+builder.Host.UseSerilog();
 
 builder.Services
     .AddLegacyData(builder.Configuration)
@@ -23,10 +27,9 @@ const string serviceName = "legacy-api";
 
 builder.Logging.AddOpenTelemetry(options =>
 {
+    options.IncludeScopes = true;
     options
-        .SetResourceBuilder(
-            ResourceBuilder.CreateDefault()
-                .AddService(serviceName))
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
         .AddConsoleExporter();
 });
 builder.Services.AddOpenTelemetry()
