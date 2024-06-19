c4a3ff8a891292ab177bbdb2e3bb9a93bcc5102a
Erik Shafer <erik.shafer@gmail.com>
2024-06-19 16:08:18 -0500

    minor tweaks to cart func cmd srvc (wording, formatting)

--- src/Retail/ShoppingCart.Api/Program.cs
+++ src/Retail/ShoppingCart.Api/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Http.Json;
 using NodaTime;
 using NodaTime.Serialization.SystemTextJson;
 using Serilog;
-using ShoppingCart;
 using ShoppingCart.Api;
 using ShoppingCart.Api.Infrastructure;
 
