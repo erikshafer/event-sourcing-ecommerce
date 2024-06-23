220fad68463da7d049f6dc9dacdd2e4eba156453
Erik Shafer <erik.shafer@gmail.com>
2024-06-22 19:50:10 -0500

    feat; began basic RabbitMQ usage with EasyNetQ

---
+++ src/Legacy/Legacy.Messages/Events/IntegrationEvent.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace Legacy.Messages.Events;
+
+public record IntegrationEvent
+{
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.UtcNow;
+    }
+
+    [JsonInclude]
+    public Guid Id { get; set; }
+
+    [JsonInclude]
+    public DateTime CreationDate { get; set; }
+}
