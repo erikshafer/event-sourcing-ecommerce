87cedf4cc9c842967be6dd85e3febc8ccf2c9ddf
Erik Shafer <erik.shafer@gmail.com>
2024-06-25 22:44:09 -0500

    simplifying CartState

--- src/Retail/ShoppingCart/Carts/CartState.cs
+++ src/Retail/ShoppingCart/Carts/CartState.cs
@@ -14,6 +14,13 @@ public record CartState : State<CartState>
 
     public bool HasProductItems => ProductItems.IsEmpty is false;
 
+    public bool CanProceedToCheckout() => Status switch
+    {
+        CartStatus.Unset => false,
+        CartStatus.Confirmed => false,
+        _ => HasProductItems
+    };
+
     public CartState()
     {
         On<CartOpened>(Handle);
@@ -33,33 +40,21 @@ public record CartState : State<CartState>
 
     private static CartState Handle(CartState state, ProductAddedToCart @event) => state.Status switch
     {
-        CartStatus.Confirmed => throw InvalidStateChangeException.For<CartState, ProductAddedToCart>(state.Id, CartStatus.Confirmed),
         _ => state with { ProductItems = state.ProductItems.Add(ProductItem.From(@event.ProductId, @event.Quantity)) }
     };
 
     private static CartState Handle(CartState state, ProductRemovedFromCart @event) => state.Status switch
     {
-        CartStatus.Confirmed => throw InvalidStateChangeException.For<CartState, ProductRemovedFromCart>(state.Id, CartStatus.Confirmed),
         _ => state with { ProductItems = state.ProductItems.Remove(ProductItem.From(@event.ProductId, @event.Quantity)) }
     };
 
     private static CartState Handle(CartState state, CartConfirmed @event) => state.Status switch
     {
-        CartStatus.Confirmed => throw InvalidStateChangeException.For<CartState, CartConfirmed>(state.Id, CartStatus.Confirmed),
         _ => state with { Status = CartStatus.Confirmed }
     };
 
     private static CartState Handle(CartState state, CartCancelled @event) => state.Status switch
     {
-        CartStatus.Confirmed => throw InvalidStateChangeException.For<CartState, CartCancelled>(state.Id, CartStatus.Confirmed),
-        CartStatus.Cancelled => throw InvalidStateChangeException.For<CartState, CartCancelled>(state.Id, CartStatus.Cancelled),
         _ => state with { Status = CartStatus.Cancelled }
-    };
-
-    public bool CanProceedToCheckout() => Status switch
-    {
-        CartStatus.Unset => false,
-        CartStatus.Confirmed => false,
-        _ => HasProductItems
     };
 }
